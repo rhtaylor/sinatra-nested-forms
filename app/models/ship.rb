2a9e8f2b2da3959c17bd1abd85013cccee48e8b3
Randy Taylor <randyisbusy@gmail.com>
2019-10-17 04:02:20 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,7 +2,8 @@ class Ship
   @@all = []
   attr_reader :name, :type, :boody
   def initialize(params)
-    
+    params["pirate"]["ships"].each do |x|
+      binding.pry
   @name = params["pirate"]["ships"][]["name"]
   @type = params["pirate"]["ships"][]["type"]
   @booty = params["pirate"]["ships"][]["booty"]
